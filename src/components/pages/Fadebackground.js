0ecba7d310051038bdd42a9d302574347c630bc6
tonyguin0123 <tonyguin0123@gmail.com>
2023-07-20 11:41:57 -0500

    Fadebackground

--- src/components/pages/Fadebackground.js
+++ src/components/pages/Fadebackground.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import "./Page.css/Fadingbackground.css";
+import "./Page.css/Fadebackground.css";
 
-const Fadingbackground = () => {
+const Fadebackground = () => {
   const [scrollOpacity, setScrollOpacity] = useState(1);
 
   useEffect(() => {
@@ -27,4 +27,4 @@ const Fadingbackground = () => {
   );
 };
 
-export default Fadingbackground;
+export default Fadebackground;
