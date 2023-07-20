9f8169d8e6064dfbe564127e3dd77423421dceb5
tonyguin0123 <tonyguin0123@gmail.com>
2023-07-20 10:58:37 -0500

    cleared cache

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+
 import Home from "./components/Pages/Home";
 
 function App() {
