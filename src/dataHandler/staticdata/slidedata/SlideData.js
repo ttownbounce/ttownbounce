350776915d582d1e77ccd073c7265004f376a92b
tonyguin0123 <tonyguin0123@gmail.com>
2023-08-17 14:17:02 -0500

    new Photos. Most of the CO

--- src/dataHandler/staticdata/slidedata/SlideData.js
+++ src/dataHandler/staticdata/slidedata/SlideData.js
@@ -12,65 +12,59 @@ function SlideData() {
         {
           id: 600,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
-          title: "ROCK CLIMB SLIDE (dual lane)",
-          price: 350
-        },
-        {
-          id: 601,
-          images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "15' RAINBOW SLIDE (dual lane)",
           price: 250
         },
         {
-          id: 602,
+          id: 601,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "19' MULTICOLOR SLIDE",
           price: 300
         },
         {
-          id: 603,
+          id: 602,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "18' YELLOW CRUSH SLIDE (single)",
           price: 250
         },
         {
-          id: 604,
+          id: 603,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "18' GREEN CRUSH SLIDE (single)",
           price: 250
         },
         {
-          id: 605,
+          id: 604,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "18' BLUE TSUNAMI (dual)",
           price: 300
         },
         {
-          id: 606,
+          id: 605,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "18' TROPICAL (single)",
           price: 250
         },
         {
-          id: 607,
+          id: 606,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "18' TROPICAL (dual)",
           price: 300
         },
         {
-          id: 608,
+          id: 607,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "20' TROPICAL SLIDE (single)",
           price: 300
         },
         {
-          id: 609,
+          id: 608,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "20' TROPICAL PARADISE SLIDE (single)",
           price: 300
         },
         {
-          id: 610,
+          id: 609,
           images: [tempCarousel1, tempCarousel2, tempCarousel3,tempCarousel4,tempCarousel5,tempCarousel6],
           title: "20' BLUE WAVE SLIDE (single)",
           price: 300
