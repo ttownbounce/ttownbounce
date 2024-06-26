513d6697d3f56d0161782ec436034cf7f348853f
Aprille Hamilton <aprille@aprilles-air.attlocal.net>
2024-06-26 12:56:16 -0500

    update new arival

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -18,8 +18,8 @@ function Home() {
   var mostPopularData = []
 
 
-  const carouselIDS = [404,408,604,814,820,999]
-  const mostPopularIDS = [200,207,999]
+  const carouselIDS = [404,408,604,814,820,821,999]
+  const mostPopularIDS = [200,207,821]
 
   carouselIDS.forEach((number) => {
     const item = GetItemById(number.toString());
